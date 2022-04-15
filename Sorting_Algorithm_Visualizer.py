0b8d62c6da466c8cffc2c8afc4938ece9eafdd75
Kooreis <kooreis.official@gmail.com>
2022-04-15 02:00:00 -0500

    Defined two functions: 'print_array' to print the elements of an array and 'bubble_sort' to sort an array using the bubble sort algorithm.

--- Sorting_Algorithm_Visualizer.py
+++ Sorting_Algorithm_Visualizer.py
@@ -1,2 +1,13 @@
-import time
-import random
+def print_array(arr):
+    for i in arr:
+        print(i, end=" ")
+    print()
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            print_array(arr)
+        print()
